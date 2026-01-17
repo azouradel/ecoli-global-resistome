d9178b505d0884a2a04b6082cdad75fad397f0ad
azouradel <adelazour3@gmail.com>
2026-01-17 17:40:27 +0100

    Delete scripts/scripts directory

--- scripts/scripts/arg_detection_example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Example command illustrating antimicrobial resistance gene detection
-# using the ResFinder database via ABRicate.
-# This script represents a simplified example of the analytical approach
-# described in the Methods section of the associated manuscript.
-
-abricate --db resfinder example_genome.fasta > example_arg_output.tsv
